f24cac6dbc0560e72fe9c844dadb709f7100f839
OttKirss <Ott.Kirss@gmail.com>
2023-12-03 19:58:29 +0200

    D2P2

--- day02/day02.ts
+++ day02/day02.ts
@@ -1,58 +1,43 @@
 import * as fs from 'fs';
 
-export function day02(redCubeCount: number, greenCubeCount: number, blueCubeCount: number) {
+export function day02() {
     const fileContents: string = fs.readFileSync('./input.txt', 'utf-8');
     let total: number = 0;
-    
-    const isValidGame = (gameRedCount: number, gameGreenCount: number, gameBlueCount: number): boolean => {
-        if (gameRedCount > redCubeCount) {
-            return false;
-        }
-        
-        if (gameGreenCount > greenCubeCount) {
-            return false;
-        }
-        
-        if (gameBlueCount > blueCubeCount) {
-            return false;
-        }
-        
-        return true;
-    }
 
     fileContents.split('\n').map((row) => {
-        let isSetOfGamesValid: boolean = true;
+        let redCount: number = 0;
+        let greenCount: number = 0;
+        let blueCount: number = 0;
+        
         row.split(':')[1].trim().split(';').map((gameSet) => {
-            let redCount: number = 0;
-            let greenCount: number = 0;
-            let blueCount: number = 0;
             gameSet.trim().split(',').map((cubesFromBag) => {
                 if (cubesFromBag.includes('green')) {
-                    greenCount = Number(cubesFromBag.replace('green', '').trim());
+                    const tempNumb: number = Number(cubesFromBag.replace('green', '').trim());
+                    if (tempNumb > greenCount) {
+                        greenCount = tempNumb;
+                    }
                 }
 
                 if (cubesFromBag.includes('red')) {
-                    redCount = Number(cubesFromBag.replace('red', '').trim());
+                    const tempNumb: number = Number(cubesFromBag.replace('red', '').trim());
+                    if (tempNumb > redCount) {
+                        redCount = tempNumb;
+                    }
                 }
 
                 if (cubesFromBag.includes('blue')) {
-                    blueCount = Number(cubesFromBag.replace('blue', '').trim());
+                    const tempNumb: number = Number(cubesFromBag.replace('blue', '').trim());
+                    if (tempNumb > blueCount) {
+                        blueCount = tempNumb;
+                    }
                 }
             });
-
-            if (!isValidGame(redCount, greenCount, blueCount)) {
-                isSetOfGamesValid = false;
-            }
         });
-        
-        if (isSetOfGamesValid) {
-            total += Number(row.split(':')[0].replace('Game', '').trim());
-        } else {
-            
-        }
+
+        total += (redCount * greenCount * blueCount);
     });
     
     console.log(total);
 }
 
-day02(12, 13, 14);
+day02();
